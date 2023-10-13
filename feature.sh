0b380f488ecc75cd242f232bd57c225fe29feb60
shashikuamar <shashikumarmandya7026@gmail.com>
2023-10-13 16:18:33 +0530

    modified feature.sh

--- feature.sh
+++ feature.sh
@@ -2,10 +2,8 @@
 
 echo "This is a feature branch"
 
+echo "eiting from merhe conflict branch
+"
 echo "Now i will merge with master"
 
-<<<<<<< HEAD
 echo "alredy merged"
-=======
-echo "alredy merge"
->>>>>>> feature
